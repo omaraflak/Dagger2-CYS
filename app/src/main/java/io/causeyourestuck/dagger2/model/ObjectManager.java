dd174b3c8a07e1c2bcacac34697eb1efaf6269e7
omaflak <aflakomar@hotmail.fr>
2017-02-18 18:15:19 +0100

    removed useless import

--- app/src/main/java/io/causeyourestuck/dagger2/model/ObjectManager.java
+++ app/src/main/java/io/causeyourestuck/dagger2/model/ObjectManager.java
@@ -1,8 +1,6 @@
 package io.causeyourestuck.dagger2.model;
 
-import android.content.Context;
 import android.content.SharedPreferences;
-import android.preference.PreferenceManager;
 
 import com.google.gson.Gson;
 
